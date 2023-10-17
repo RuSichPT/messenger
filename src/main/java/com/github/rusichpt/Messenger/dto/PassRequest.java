b6f8c9841befd89786239b40276a887b18321274
pavel.tokarev <tokarevpavel1996@mail.ru>
2023-10-17 12:47:24 +0300

    Исправил JSON parse ERROR

--- src/main/java/com/github/rusichpt/Messenger/dto/PassRequest.java
+++ src/main/java/com/github/rusichpt/Messenger/dto/PassRequest.java
@@ -3,8 +3,12 @@ package com.github.rusichpt.Messenger.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 @Data
+@NoArgsConstructor(force = true)
+@RequiredArgsConstructor
 public class PassRequest {
     @NotNull
     @NotBlank
